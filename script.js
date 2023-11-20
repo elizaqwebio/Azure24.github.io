c7450c2e6a765a931d7e238b184db9fd464ee9ff
elizaqwebio <150945940+elizaqwebio@users.noreply.github.com>
2023-11-20 16:12:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+var menubtn = document.getElementById("menubtn");
+var sidebar = document.getElementById("sidebar");
+
+sidebar.style.right = "-250px";
+menubtn.onclick = function () {
+    if (sidebar.style.right == "-250px") {
+        sidebar.style.right = "0";
+    }
+    else {
+        sidebar.style.right = "-250px";
+    }
+}
+
+var swiper = new Swiper(".typeSwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 400,
+        modifier: 2,
+        slideShadows: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+
+
+function clickStart() {
+    window.location = "ons.html";
+}
+
+function clickGoNow() {
+    window.location = "sids.html";
+}
